55c83aa7c034bd7541fb46436e10b2cddfd56363
Sviridov Maksim <yes-99@mail.ru>
2024-06-17 21:22:59 +0700

    Update Task3.sql

--- Task3.sql
+++ Task3.sql
@@ -1,4 +1,4 @@
 SELECT Products.ProductName, Categories.CategoryName
 FROM Products
-LEFT JOIN Categories ON Products.CategoryID = Categories.CategoryID
-ORDER BY Products.ProductName
+LEFT JOIN ProductCategories ON Products.ID = ProductCategories.ProductId
+LEFT JOIN Categories ON ProductCategories.CategoryID = Categories.ID
